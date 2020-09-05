71f4e16397debad59691edca1d0e2039d5504786
johnwmarrs <johnwmarrs@tamu.edu>
2020-09-05 15:06:55 -0600

    Added terminal frame

--- TerminalFrame.py
+++ TerminalFrame.py
@@ -4,18 +4,26 @@ class TerminalFrame(tk.Frame):
 	def __init__(self, parent=None):
 		self.fontMedium = ('Verdana', 14)
 
-		tk.Frame.__init__(self, parent, bg="red")
+		tk.Frame.__init__(self, parent, bg="white")
 		self.pack(fill="both", expand=True)
 		self.makeWidgets()
 
 	def makeWidgets(self):
 
-		textArea = tk.Text(self, bg="black", fg="green", font=self.fontMedium)
+		topFrame = tk.Frame(self)
+
+		scrollbar  = tk.Scrollbar(topFrame)
+		scrollbar.pack(side="right", fill="y")
+
+		textArea = tk.Text(topFrame, bg="black", fg="green", font=self.fontMedium, yscrollcommand=scrollbar.set)
+		textArea.pack(side="left", expand=True, fill="both")
+
+		scrollbar.config(command=textArea.yview)
 
 		entryFieldTextVar = tk.StringVar()
 		entryFieldTextVar.set("c:/reveille/home>")
 		entryField = tk.Entry(self, bg="black", fg="green", font=self.fontMedium, textvariable=entryFieldTextVar)
 
 		entryField.pack(fill="x", side="bottom")
-		textArea.pack(side="top", fill="both", expand=True)
+		topFrame.pack(side="top", fill="both", expand=True, pady=(0, 5))
 
