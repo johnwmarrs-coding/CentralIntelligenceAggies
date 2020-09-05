6b3df593820eb4ed76ccfbd9a0ecda359cec43d8
johnwmarrs <johnwmarrs@tamu.edu>
2020-09-05 12:56:58 -0600

    Fixed bad file again lol

--- TestApp.py
+++ TestApp.py
@@ -1,10 +1,8 @@
 #Main application to test image viewer
 
 import tkinter as tk
-import ImageViewer as iv
 root = tk.Tk()
 root.geometry("500x500")
 
-viewer = iv.ImageViewerFrame(root)
 
 root.mainloop()
