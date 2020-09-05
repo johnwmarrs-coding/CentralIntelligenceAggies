d12008db5a91c5d41b70e51f245fedf6238c9634
chrisplmmr <57369942+chrisplmmr@users.noreply.github.com>
2020-09-05 17:22:06 -0500

    Add files via upload

--- encryption.py
+++ encryption.py
@@ -2,10 +2,18 @@ import challenges
 
 
 class Encryption(challenges.Challenge):
+    # Variables
+    points = 10
+    prompt = "You suspect that somobody might be listening to this message...\nEncrypt this message with Caesar " \
+             "Cipher to hide this message... 'Sombody has the password to the hackathon site, please change it.' "
+    ans = "Xtrgtid mfx ymj ufxxbtwi yt ymj mfhpfymts xnyj, uqjfxj hmfslj ny."
 
-    #Variables
+    # Constructor
+    def getPrompt(self):
+        return self.prompt
 
-    #Constructor
-
-    def runchal(self):
-        print("### Encryption Challenge ###")
+    def getResult(self, userAns):
+        if userAns.lower() == self.ans.lower():
+            return True
+        else:
+            return False
