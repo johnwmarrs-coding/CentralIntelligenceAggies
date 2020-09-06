3a41d2aa116b9ef98c8ef92007c56c4176cba428
chrisplmmr <57369942+chrisplmmr@users.noreply.github.com>
2020-09-05 19:43:47 -0500

    Add files via upload

--- maninmiddle.py
+++ maninmiddle.py
@@ -4,8 +4,29 @@ import challenges
 class ManInMiddle(challenges.Challenge):
 
     # Variables
+    prompt = "Its your boss again ... we have intercepted Bevo's messages sent to a 2 percenter. We need you to " \
+             "perform decryption again and steal his login information." \
 
-    # Constructor
+    points = 0
 
-    def runchal(self):
-        print("### ManInMiddle Challenge ###")
+    interceptMessage = "Odxqfk wkh udqvrpzduh"
+
+    decryptedIntercept = "Launch the ransomware"
+
+    def getIntercept(self):
+        return self.interceptMessage
+
+    # Call this after get result because points get set after get result
+    def getPoints(self):
+        return self.points
+
+    def computeResult(self, userResult):
+        if self.decryptedIntercept.lower == userResult.lower:
+            self.points == 10
+
+    def getResult(self, userResult):
+        if self.decryptedIntercept.lower == userResult.lower:
+            self.computeResult(userResult)
+            return True
+
+
