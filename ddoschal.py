e5fe3fa7e578778462cbf7962c5a39433d03637b
johnwmarrs <johnwmarrs@tamu.edu>
2020-09-05 18:23:33 -0600

    Added ddoschal and sqlchal

--- ddoschal.py
+++ ddoschal.py
@@ -4,8 +4,47 @@ import challenges
 class DDOSChallenge(challenges.Challenge):
 
     #Variables
+    points = 10
+
+    prompt = "Users are reporting that they cannot access the howdyhack website for registration. Can you look into the issue? It might be a DOS attack. Maybe you can block some IPs"
+
+    ipsToBlacklist = ["153.65.230.21", "301.78.712.11", "120.78.712.31"]
+
+    keyInfo = "Attacker IPs = '153.65.230.21', '301.78.712.11', '120.78.712.31'"
 
     #Constructor
 
     def runchal(self):
         print("### DDOS Challenge ###")
+
+    def getPrompt(self):
+    	return self.prompt
+
+    def getResult(self, userAnswer):
+    	hits = 0
+    	misses = 0
+
+    	tempIpsToBlacklist = self.ipsToBlacklist.copy()
+    	for ip in userAnswer:
+    		if(ip in tempIpsToBlacklist):
+    			tempIpsToBlacklist.remove(ip)
+    			hits += 1
+
+    		else:
+    			misses += 1
+
+    	pointsEarned = 0
+    	if (hits == 3):
+    		pointsEarned = 6
+    	elif (hits == 2):
+    		pointsEarned = 4
+    	elif (hits == 1):
+    		pointsEarned = 2
+
+    	if (misses == 0):
+    		pointsEarned += 4
+
+    	return pointsEarned
+
+    def getKeyInfo(self):
+    	return self.keyInfo
--- sqlchal.py
+++ sqlchal.py
@@ -5,7 +5,35 @@ class SQLInject(challenges.Challenge):
 
     # Variables
 
+    prompt = "Perhaps you can perform SQLInjection on the suspected website to discover the true identity of the person hacking us."
+    resource = "https://www.w3schools.com/sql/sql_injection.asp"
+
+    response = ("index, firstName, lastName, username\n0, Charles, Barkley, cbarkley1\n1, Jordan, Michaels, theBigJorms\n"
+    "2, Hannah, Hannadotir, theRealHannah\n" 
+    "3, George, Andwell, animalFarmed2\n" 
+    "4, Henrietta, Andrews, henandrews1998\n" 
+    "5, Bevo, Longhorn, bvbandit1\n"
+    "6, Rebecca, Beckinson, Rebeckinson420"
+    )
+
+
+    username = 'bvbandit1'
+    keyInfo = 'Bevo Longhorn'
+    answer = "OR 1=1" 
+
     # Constructor
 
-    def runchal(self):
-        print("### Sql Injection Challenge ###")
+    def getPrompt(self):
+        return self.prompt
+
+    def getResult(self, userAnswer):
+    	if (self.answer.replace(" ", "").lower() == userAnswer.replace(" ", "").lower()):
+    		return 10
+    	else:
+    		return 0
+
+    def getResource(self):
+    	return self.resource
+
+    def getResponse(self):
+    	return self.response
