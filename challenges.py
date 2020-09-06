a0fb679c9849848a303240c8e5f45d7b2d88a516
chrisplmmr <57369942+chrisplmmr@users.noreply.github.com>
2020-09-05 23:51:06 -0500

    Update challenges.py

--- challenges.py
+++ challenges.py
@@ -1,7 +1,5 @@
 # File: challenges.py
-# Authors: Christopher Plummer, ...
 # Date: 09/05/2020
-# Hackathon 2020
 # Description: Contains the classes for each game challenge
 # The content of this file implements the code for a cache simulator program
 
